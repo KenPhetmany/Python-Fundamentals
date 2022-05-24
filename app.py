9c9cc5b43b43064461c546a2418687ce48088a1f
Ken Phetmany <kenphetmanydev@gmail.com>
2022-05-24 18:46:07 +1000

    Added set fundamentals

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ mountains = [
 # highest_mountains = [mountain for mountain in mountains if mountain[1] > 8600]
 
 # print(highest_mountains)
-
+'''
 person = {
     'first_name': 'John',
     'last_name': 'Doe',
@@ -30,3 +30,44 @@ person = {
 
 person['age'] = 40
 print(person.items())
+'''
+'''
+stocks = {
+    'AAPL': 121,
+    'AMZN': 3380,
+    'MSFT': 219,
+    'BIIB': 280,
+    'QDEL': 266,
+    'LVGO': 144
+}
+
+# new_stocks = {}
+
+# for symbol, price in stocks.items():
+#    new_stocks[symbol] = price*1.02
+
+# new_stocks = {symbol: price*1.02 for (symbol, price) in stocks.items()}
+
+exxy_stocks = {symbol: price for (
+    symbol, price) in stocks.items() if price > 200}
+print(exxy_stocks)
+'''
+
+'''
+tags = {'Django', 'Pandas', 'Numpy'}
+
+# lowercase_tags = set()
+# for tag in tags:
+#    lowercase_tags.add(tag.lower())
+
+lowercase_tags = {tag.lower() for tag in tags}
+
+print(lowercase_tags)
+
+'''
+s1 = {'Python', 'Java'}
+s2 = {'C#', 'Java'}
+
+s_union = s1.union(s2)
+
+print(s_union)
---
+++ sets.py
@@ -0,0 +1,21 @@
+# Union Sets
+
+'''
+s1 = {'Python', 'Java'}
+s2 = {'C#', 'Java'}
+
+s_union = s1.union(s2)
+
+print(s_union)
+'''
+
+# Intersections
+
+'''
+s1 = {'Python', 'Java', 'C++'}
+s2 = {'C#', 'Java', 'C++'}
+
+s_intersection = s1.intersection(s2)
+
+print(s_intersection)
+'''
